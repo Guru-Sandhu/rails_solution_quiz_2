593ffd8d58a35b472872eb72be84087aa89b8aa2
Guru-Sandhu <arsh3874@gmail.com>
2020-05-11 13:57:48 -0700

    added full crud and added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+
+50.times do
+  Post.create(
+    author: Faker::Name.name,
+    body: Faker::Quote.matz
+  )
+end
